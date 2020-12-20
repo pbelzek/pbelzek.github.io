5cbec5debd5dfb7c374a767f6fbe0b07fc423d71
Paweł Bełżek <belzek.pawel@gmail.com>
2020-12-20 17:43:27 +0100

    Fix date bug and ang Open Graph tags

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function(){
-  const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const now = new Date();
-  document.getElementById("day-of-week").innerText = weekdays[now.getDay() - 1];
+  document.getElementById("day-of-week").innerText = weekdays[now.getDay()];
 }());

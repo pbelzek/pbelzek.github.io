1d52c284201969533050dcfe0bd77fb87ea4ad1b
Paweł Bełżek <belzek.pawel@gmail.com>
2021-10-06 23:37:52 +0200

    Remove plaintext email from code

--- index.js
+++ index.js
@@ -3,3 +3,11 @@
   const now = new Date();
   document.getElementById("day-of-week").innerText = weekdays[now.getDay()];
 }());
+
+function onShowEmailClick() {
+  const emailSource = "YmVsemVrLnBhd2VsQGdtYWlsLmNvbQ==";
+  const emailLink = document.getElementById("email-link");
+  emailLink.setAttribute("href", `mailto:${atob(emailSource)}`)
+  emailLink.innerText = atob(emailSource);
+  emailLink.className = "email-show";
+}
